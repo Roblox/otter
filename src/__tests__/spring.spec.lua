b6cccb2d3eebcdaa44af516b9377023970033320
Anonymous Author <roflection-bot@roblox.com>
2023-02-09 16:12:18 -0800

    CLIXBOX-2753 Support stiffness, damping and mass spring parameters (#44)
    * Add translation from stiffness, damping and mass to natural frequency and damping ratio
    
    * Update spring.spec.lua
    
    * Update spring.spec.lua
    
    * move translation into a function
    
    * Update docs
    
    * Update SpringAnimationOtter.rbxl
    
    * Added tests
    
    * Update spring.spec.lua
    
    * Update spring.spec.lua

--- src/__tests__/spring.spec.lua
+++ src/__tests__/spring.spec.lua
@@ -6,6 +6,8 @@ local it = JestGlobals.it
 local describe = JestGlobals.describe
 local expect = JestGlobals.expect
 
+local abs = math.abs
+
 local spring = require(script.Parent.Parent.spring)
 
 it("should have all expected APIs", function()
@@ -26,6 +28,59 @@ it("should have all expected APIs", function()
 
 	expect(s).toEqual(expect.any("table"))
 	expect(s.step).toEqual(expect.any("function"))
+end)
+
+it("should take damping, stiffness and mass as parameters", function()
+	local s = spring(1, {
+		damping = 20,
+		stiffness = 400,
+		mass = 1,
+	})
+
+	expect(s).toEqual(expect.any("table"))
+	expect(s.step).toEqual(expect.any("function"))
+end)
+
+it("create two smiliar springs with different set of params, steps should be similar", function()
+	local s1 = spring(1, {
+		dampingRatio = 0.5,
+		frequency = 3.183,
+	})
+
+	local s2 = spring(1, {
+		damping = 20,
+		stiffness = 400,
+		mass = 1,
+	})
+
+	local state = {
+		value = 0,
+		velocity = 0,
+		complete = false,
+	}
+	local state_s1 = s1:step(state, 0.05)
+	local state_s2 = s2:step(state, 0.05)
+
+	expect(abs(state_s1.value - state_s2.value)).toBeLessThan(0.001)
+	expect(abs(state_s1.velocity - state_s2.velocity)).toBeLessThan(0.001)
+	expect(state_s1.complete).toBe(false)
+	expect(state_s2.complete).toBe(false)
+
+	state_s1 = s1:step(state, 0.5)
+	state_s2 = s2:step(state, 0.5)
+
+	expect(abs(state_s1.value - state_s2.value)).toBeLessThan(0.001)
+	expect(abs(state_s1.velocity - state_s2.velocity)).toBeLessThan(0.001)
+	expect(state_s1.complete).toBe(false)
+	expect(state_s2.complete).toBe(false)
+
+	state_s1 = s1:step(state_s1, 1)
+	state_s2 = s2:step(state_s2, 1)
+
+	expect(state_s1.value).toEqual(state_s2.value)
+	expect(state_s1.velocity).toEqual(state_s2.velocity)
+	expect(state_s1.complete).toBe(true)
+	expect(state_s2.complete).toBe(true)
 end)
 
 it("should handle being still correctly", function()
