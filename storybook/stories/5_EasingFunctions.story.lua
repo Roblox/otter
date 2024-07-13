d44ee10e9648cd38a19c97f359b372db73362bc7
Andrea Fletcher <afletcher@roblox.com>
2024-07-13 01:45:14 -0400

    Fix story

--- storybook/stories/5_EasingFunctions.story.lua
+++ storybook/stories/5_EasingFunctions.story.lua
@@ -3,31 +3,57 @@ local ReactOtter = require(Packages._Workspace.ReactOtter.ReactOtter)
 local React = require(Packages._Workspace.ReactOtter.React)
 local Dash = require(Packages._Workspace.ReactOtter.Dash)
 
+local PROGRESS_TRANSPARENCY = 0.5
+local OFFSET = 0.0001
+
 local function ToggleTextSize(easingStyle: Enum.EasingStyle)
 	local toggled, setToggled = React.useState(false)
-	local value, setGoal = ReactOtter.useAnimatedBinding(8, function(value: number)
+	local value, setGoal = ReactOtter.useAnimatedBinding(0, function(value: number)
 		print("Completed animation!", easingStyle)
 	end)
 
 	React.useEffect(function()
-		setGoal(ReactOtter.ease(if toggled then 24 else 8, {
+		setGoal(ReactOtter.ease(if toggled then 1 else 0, {
+			duration = 2,
 			easingStyle = easingStyle,
 		}))
 	end, { toggled })
 
 	return React.createElement("TextButton", {
-		Text = "Hello",
-		TextSize = value,
+		Text = easingStyle.Name,
+		TextSize = 22,
 		Size = UDim2.new(0, 200, 0, 50),
+		BackgroundColor3 = Color3.fromRGB(13, 160, 160),
 		[React.Event.Activated] = function()
 			setToggled(not toggled)
 		end,
+	}, {
+		React.createElement("UIGradient", {
+			Transparency = value:map(function(percentage: number)
+				if percentage == 1 then
+					return NumberSequence.new(0)
+				elseif percentage == 0 then
+					return NumberSequence.new(0.5)
+				end
+
+				local t1 = math.max(OFFSET, percentage - OFFSET)
+				local t2 = percentage
+				print(0, t1, t2, 1)
+		
+				return NumberSequence.new({
+					NumberSequenceKeypoint.new(0, 0),
+					NumberSequenceKeypoint.new(t1, 0),
+					NumberSequenceKeypoint.new(t2, PROGRESS_TRANSPARENCY),
+					NumberSequenceKeypoint.new(1, PROGRESS_TRANSPARENCY),
+				})
+			end),
+		})
 	})
 end
 
 return {
 	name = "Easing Functions",
-	summary = "A button that animates the size of its text when clicked based on a particular easing style",
+	summary = "A frame that animates the background in based on a particular easing style when clicked",
 	stories = Dash.map(Enum.EasingStyle:GetEnumItems(), function(easingStyle)
 		return {
 			name = easingStyle.Name,
